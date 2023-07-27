52d87b5aea92f6f95fc3bbf92111682b6d9eca74
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-07-27 14:18:06 +0700

    Add Layout

---
+++ app/(site)/page.tsx
@@ -0,0 +1,24 @@
+import { Header, ListItem } from "@/components";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+          <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
+        </div>
+      </Header>
+
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="texct-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+
+        <div className="">List of Songs!</div>
+      </div>
+    </div>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,9 @@
+import Sidebar from "./Sidebar";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
+import Header from "./Header";
+import Button from "./Button";
+import ListItem from "./ListItem";
+
+export { Sidebar, Box, SidebarItem, Library, Header, Button, ListItem };
